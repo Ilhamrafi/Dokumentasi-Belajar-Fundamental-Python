a5d2718248b3e707028ff9cad2b8b64606af5722
Ilham Rafiedhia Pramutighna <84080962+Ilhamrafi@users.noreply.github.com>
2023-12-17 22:58:19 +0700

    Add files via upload

---
+++ ifELse.py
@@ -0,0 +1,21 @@
+# if else statment 
+
+# if 
+# kondisi
+# aksi
+
+nama = input("siapa teman kamu? : ")
+
+if nama == "cahyo" :
+  print(f"{nama} dia adalah teman kamu")
+  
+elif nama == "adit" : 
+  print(f"{nama} dia adalah teman kamu")
+  
+elif nama == "aulia" :  
+  print(f"{nama} dia adalah teman kamu")
+  
+else :
+  print(f"{nama} bukan teman kamu")
+
+print("selesai")
